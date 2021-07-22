6705142f13cbab5776e2bf625be1261cc8157cc4
DEDDY389 <noviadideddy@gmail.com>
2021-07-22 10:17:52 +0700

    penambahan di views transaksi

--- app/Views/transaksi/sptjb.php
+++ app/Views/transaksi/sptjb.php
@@ -9,7 +9,6 @@
 
         td,
         th {
-            border: 1px solid #000000;
             text-align: center;
         }
     </style>
@@ -24,15 +23,24 @@
         Nomor : <?= $pembeli->username ?>
     </div>
     <div style="text-align: justify;">
-        Sehubungan dengan Surat Perintah Pembayaran Langsung (SPP-LS) Nomor : <?= $pembeli->username ?> Tanggal <?= date('Y-m-d', strtotime($transaksi->created_date)) ?> yang kami ajukan sebesar <?= "Rp" . number_format($transaksi->total_harga, 2, ',', '.') ?> (<?= terbilang("$transaksi->total_harga") ?>) Untuk Keperluan SKPD Rumah Sakit Umum Daerah (RSUD) Berkah Kabupaten Pandeglang Tahun Anggaran 2021, dengan ini menyatakan dengan sebenarnya bahwa :<br>
+        Sehubungan dengan Surat Perintah Pembayaran Langsung (SPP-LS) Nomor : <?= $pembeli->username ?> Tanggal <?= date('d-m-Y', strtotime($transaksi->created_date)) ?> yang kami ajukan sebesar <?= "Rp" . number_format($transaksi->total_harga, 2, ',', '.') ?> (<?= terbilang("$transaksi->total_harga") ?>) Untuk Keperluan SKPD Rumah Sakit Umum Daerah (RSUD) Berkah Kabupaten Pandeglang Tahun Anggaran 2021, dengan ini menyatakan dengan sebenarnya bahwa :<br>
         1. Jumlah Langsung (LS) tersebut diatas akan digunakan untuk keperluan guna membiayai kegiatan yang akan kami laksanakan sesuai DPA-SKPD.<br>
         2. Jumlah Langsung (LS) tersebut tidak akan digunakan untuk membiayai pengeluaran-pengeluaran yang menurut ketentuan yang berlaku harus dilakukan dengan pembayaran Langsung (LS).<br>
         Demikian surat pernyataan ini dibuat untuk melengkapi persyaratan pengajuan SPP-LS SKPD kami.<br>
-        Mengetahui,<br>
-        Pengguna Anggaran/Kuasa Pengguna Anggaran<br>
-        dr. Cut Budiarti Emansada<br>
-        Pembina / IV.a<br>
-        Nip. 19700729 200911 2 001
+        <p></p>
+        <table>
+            <tr>
+                <td></td>
+                <td>Mengetahui,<br>
+                    Pengguna Anggaran/Kuasa Pengguna Anggaran<br>
+                    <p></p>
+                    <p></p>
+                    dr. Cut Budiarti Emansada<br>
+                    Pembina / IV.a<br>
+                    Nip. 19700729 200911 2 001
+                </td>
+            </tr>
+        </table>
     </div>
 </body>
 
